a993b6aa13f69c421d88cdabaef7e29b053669c6
ruam <ruamfg@gmail.com>
2011-10-13 21:53:20 -0300

    Primeiros testes

--- src/JogoDaVelha.java
+++ src/JogoDaVelha.java
@@ -1,14 +1,40 @@
 public class JogoDaVelha {
 
+		
 	public static String[][] inicializar() {
-		return null;
+		String tabuleiro [][] = {{"", "", ""}, 
+								 {"", "", ""}, 
+								 {"", "", ""}};
+		
+		return tabuleiro;
 	}
 
 	public static void jogar(String[][] tabuleiro, int i, int j, String jogador) throws JogoDaVelhaException {
-
+		    
+		
+		verificaSeExiste(tabuleiro, i, j, jogador);
+		    	 
+		
+		     
+		if(tabuleiro[i][j].equals("")){
+			tabuleiro[i][j]=jogador;
+		} else if(!tabuleiro[i][j].equals("")){
+			throw new JogoDaVelhaException("Ja esta marcado!");
+			
+		}
+	
 	}
-
+	public static void verificaSeExiste(String[][] tabuleiro, int i, int j, String jogador) throws JogoDaVelhaException {
+		if(i>=tabuleiro.length||j>=tabuleiro.length){
+			throw new JogoDaVelhaException("Ja esta marcado!");
+		}
+	}
+	
+	
+	
+	
 	public static boolean existeGanhador(String[][] tabuleiro, String jogador) {
 		return false;
 	}
+	
 }
